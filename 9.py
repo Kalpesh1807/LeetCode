a9ccfabc1c82ba635086f6be55bff22ebca2b6aa
Kalpesh1807 <kalpeshkiranbhai@gmail.com>
2026-08-04 21:46:50 +0530

    added Leed code Q9

---
+++ 9.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if( x < 0):
+            return False
+        i = x 
+        s = 0
+        while (i > 0):
+            d= i % 10
+            s = s * 10 + d
+            i = i//10
+        if  (x == s):
+            return True
+    
+print(Solution().isPalindrome(121));
+print(Solution().isPalindrome(-101)); 
+print(Solution().isPalindrome(123));

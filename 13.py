41f3508f71842544ab447dceacd96a785e6144e1
Kalpesh1807 <kalpeshkiranbhai@gmail.com>
2026-08-05 10:54:18 +0530

    added file 13.py

---
+++ 13.py
@@ -0,0 +1,38 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_dict = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+
+        total = 0
+        n= len(s)
+
+        for i in range(0 , n):
+            if i < n - 1 and roman_dict[s[i]] < roman_dict[s[i + 1]]:
+                total -= roman_dict[s[i]]
+            else :
+                total += roman_dict[s[i]]
+        return total
+
